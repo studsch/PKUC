5b207187632740ef7adbd1546a8db7370496b16a
studsch <113303183+studsch@users.noreply.github.com>
2022-10-31 20:06:08 +0700

    Delete practice5 directory

--- practice5/src/main/java/main/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package main.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
